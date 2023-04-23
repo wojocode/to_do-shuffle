0ffbaeca02ee8467a576b6009a53bf61784c9f7e
Wojciech_Natkaniec <Wojciech665@gmail.com>
2023-04-24 00:11:02 +0200

    add register feature

--- app.py
+++ app.py
@@ -1,13 +1,58 @@
+import os
+from tempfile import mkdtemp
 from cs50 import SQL
-from flask import Flask, request, redirect, url_for, render_template
+from flask import Flask, request, redirect, url_for, render_template,flash, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 
 # configure session (server-side)
-app.config['SESSION TYPE'] = 'filesystem'
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-#connect database 
-db = SQL("sqlite:///database.db")
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+#connect database
+db = SQL("sqlite:///data.db")
+
+#REGISTER USER#
+@app.route('/register', methods=["POST", "GET"])
+def register():
+    username = request.form.get("username")
+    password = request.form.get("password")
+    confirmation = request.form.get("confirmation")
+    
+    if request.method == "POST":
+# ensure proper inputs 
+        if not username or not password:
+            flash("missing username or password!","danger")
+            return render_template('register.html')
+        elif password != confirmation:
+            flash("password dosn't match!","danger")
+            return render_template('register.html')
+
+    # check for free username
+        rows = db.execute("SELECT username FROM users WHERE username = ?;",username)
+        if len(rows) == 1:
+            flash("username is not available!","danger")
+            return render_template('register.html')
+        else:
+            hash = generate_password_hash(password)
+            db.execute("INSERT INTO users (username,hash) VALUES (?,?);", username,hash)
+            rows = db.execute("SELECT * FROM users WHERE username = ?;",username)
+            session["user_id"] = rows[0]["id"]
+            return "okay"
+# GET method            
+    else:
+        return render_template("register.html")
+        
+        
---
+++ static/index.js
@@ -0,0 +1,31 @@
+// display last modification
+
+if (document.querySelector(".last_mod")){
+    let LastModif = new Date(document.lastModified);
+    document.querySelector(".last_mod").innerHTML = "last mod: " + LastModif.toDateString();
+    };
+    
+// bluring page on nav button click
+    function blur(selector,value){
+        let matches = document.querySelector(selector);
+        matches.style.opacity = value;
+    }
+    
+    let bluring = 1;
+    const identy = ".container";
+    const low = "0.4";
+    const high = "1";
+    
+    document.querySelector(".navbar-toggler").addEventListener('click', function(){
+        switch(bluring){
+            case 1:
+                blur(identy,low);
+                bluring = 0
+                break;
+    
+            case 0:
+                blur(identy,high)
+                bluring = 1;
+                break;
+            }
+    });
