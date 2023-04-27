15549bc7589f05e5e5a5a16e25ecadbdc1faae21
Wojciech_Natkaniec <Wojciech665@gmail.com>
2023-04-27 22:11:03 +0200

    add category counter feature

--- app.py
+++ app.py
@@ -26,15 +26,12 @@ def after_request(response):
     return response
 
 
-@app.route('/', methods = ["GET", "POST"])
+
+
+@app.route('/',)
 @login_required
 def index():
-    if request.method == "POST":
-# check inputs 
-        task = request.form.get("task")
-        category = request.form.get("category")
-        date = request.form.get("date")
-        
+ 
 # selecting records and pass it to display 
         rows = db.execute("SELECT task,category,created_date,due_date FROM tasks WHERE user_id IN (SELECT id FROM users WHERE id = ?);",session["user_id"])
 # SELECT COUNT of each category
@@ -44,24 +41,8 @@ def index():
         
         personal = db.execute("SELECT COUNT(category) AS personal FROM tasks GROUP BY category HAVING category = 'Personal' AND user_id IN (SELECT id FROM users WHERE id = ?);",session["user_id"])
         
-        if not task or not category or not date:
-            flash("Must provide data!","danger")
-            return render_template("index.html",records = rows,h = house,w = work,p = personal)
-        
-# insert task 
-        db.execute("INSERT INTO tasks (task,category,created_date,due_date,user_id) VALUES(?,?,DATE(),?,?);", task,category,date,session["user_id"])
-    
-        flash("Added task!","success")
         return render_template("index.html",records = rows,h = house,w = work,p = personal)
-    else:
-        rows = db.execute("SELECT task,category,created_date,due_date FROM tasks WHERE user_id IN (SELECT id FROM users WHERE id = ?);",session["user_id"])
-        
-        house = db.execute("SELECT COUNT(category) AS house FROM tasks GROUP BY category HAVING category = 'House' AND user_id IN (SELECT id FROM users WHERE id = ?);",session["user_id"])
-        
-        work = db.execute("SELECT COUNT(category) AS work FROM tasks GROUP BY category HAVING category = 'Work' AND user_id IN (SELECT id FROM users WHERE id = ?);",session["user_id"])
-        
-        personal = db.execute("SELECT COUNT(category) AS personal FROM tasks GROUP BY category HAVING category = 'Personal' AND user_id IN (SELECT id FROM users WHERE id = ?);",session["user_id"])
-        return render_template("index.html", records= rows,h = house,w = work, p = personal)
+
     
 @app.route('/edit<task>:<category>:<data>', methods = ["GET", "POST"])
 @login_required
@@ -71,6 +52,24 @@ def edit(task,category,data):
         return render_template("edit.html",task = task, category = category,data = data,checked= checked)  
     else:
         return redirect("/")
+
+
+@app.route('/add' ,methods = ["POST", "GET"])
+@login_required
+def add():
+        task = request.form.get("task")
+        category = request.form.get("category")
+        date = request.form.get("date")
+        
+        if not task or not category or not date:
+            flash("Must provide data!","danger")
+            return redirect("/")
+
+# insert task 
+        db.execute("INSERT INTO tasks (task,category,created_date,due_date,user_id) VALUES(?,?,DATE(),?,?);", task,category,date,session["user_id"])
+        flash("Added task!","success")
+        return redirect('/')
+
 
 #REGISTER USER#
 @app.route('/register', methods=["POST", "GET"])
