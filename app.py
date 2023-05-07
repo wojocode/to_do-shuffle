5bf6b67c6610cb035a5df6789a23a92041276118
Wojciech_Natkaniec <Wojciech665@gmail.com>
2023-05-07 20:26:40 +0200

    develop history page and register validation

--- app.py
+++ app.py
@@ -17,7 +17,6 @@ Session(app)
 #connect database
 db = SQL("sqlite:///data.db")
 
-
 @app.after_request
 def after_request(response):
     """Ensure responses aren't cached"""
@@ -30,25 +29,25 @@ def after_request(response):
 @login_required
 def index():
 # display active records 
-        rows = db.execute("SELECT task,category,created_date,due_date,id FROM tasks WHERE user_id IN (SELECT id FROM users WHERE id = ?) AND id IN (SELECT task_id FROM archieve WHERE status = 'active');",session["user_id"])
-        
+        rows = db.execute("SELECT task,category,created_date,due_date,id FROM tasks WHERE user_id IN (SELECT id FROM users WHERE id = ?) AND id IN (SELECT task_id FROM archieve WHERE status = 'active');",session["user_id"])  
 # pass to html history records 
-        history_rows = db.execute("SELECT * FROM archieve INNER JOIN tasks ON tasks.id = archieve.task_id WHERE tasks.user_id = ? AND archieve.status != 'active';",session["user_id"])
-        
+        history_rows = db.execute("SELECT * FROM archieve INNER JOIN tasks ON tasks.id = archieve.task_id WHERE tasks.user_id = ? AND archieve.status != 'active';",session["user_id"])   
 # SELECT COUNT of each category
-        house = db.execute("SELECT COUNT(category) AS house FROM tasks WHERE category = 'House' AND user_id IN (SELECT id FROM users WHERE id = ?) AND id IN (SELECT task_id FROM archieve WHERE status = 'active');",session["user_id"])
-        
+        house = db.execute("SELECT COUNT(category) AS house FROM tasks WHERE category = 'House' AND user_id IN (SELECT id FROM users WHERE id = ?) AND id IN (SELECT task_id FROM archieve WHERE status = 'active');",session["user_id"])  
         work = db.execute("SELECT COUNT(category) AS work FROM tasks WHERE category = 'Work' AND user_id IN (SELECT id FROM users WHERE id = ?) AND id IN (SELECT task_id FROM archieve WHERE status = 'active');",session["user_id"])
-        
         personal = db.execute("SELECT COUNT(category) AS personal FROM tasks WHERE category = 'Personal' AND user_id IN (SELECT id FROM users WHERE id = ?) AND id IN (SELECT task_id FROM archieve WHERE status = 'active');",session["user_id"])
-        
+# display shuffle function when user have more than 1 task
         if len(rows) >= 2:
             shuffle = True
         else:
             shuffle = False
-        
-        return render_template("index.html",records = rows,h = house,w = work,p = personal,hist_row = history_rows,shuffle = shuffle)
-
+ # hide/display history button depends on task quantity        
+        if len(history_rows) < 1:
+            history = False
+        else:
+            history = True
+    
+        return render_template("index.html",records = rows,h = house,w = work,p = personal,history_rows = history,shuffle = shuffle)
 
 #REGISTER USER#
 @app.route('/register', methods=["POST", "GET"])
@@ -56,21 +55,22 @@ def register():
     username = request.form.get("username")
     password = request.form.get("password")
     confirmation = request.form.get("confirmation")
-    
     if request.method == "POST":
 # ensure proper inputs 
         if not username or not password:
             flash("Missing username or password!","danger")
             return render_template('register.html')
+# validate username 
+        if username.isspace() or password.isspace():
+            flash("Invalid username or password !","danger")
+            return render_template('register.html')
 # password validation
         elif validate_password(password) == False:
             flash("Password is too short (at least 8 characters)!","danger")
             return render_template('register.html')
-        
         elif password != confirmation:
             flash("Password doesn't match!","danger")
             return render_template('register.html')
-
     # check for free username
         rows = db.execute("SELECT username FROM users WHERE username = ?;",username)
         if len(rows) == 1:
@@ -85,7 +85,6 @@ def register():
             return redirect("/")           
     else:
         return render_template("register.html")
-
 
 @app.route('/login', methods = ["POST", "GET"])
 def login():
@@ -109,33 +108,28 @@ def login():
     else:
         return render_template("login.html")
     
-    
 @app.route('/logout', methods = ["GET", "POST"])
 def logout():   
     session.clear()
     flash("Logout succesfull!", "warning")
     return redirect("/")
- 
-        
+   
 @app.route('/<category>')
+@login_required
 def house(category):
     cat = category
     row = db.execute("SELECT task,category,created_date,due_date,id FROM tasks WHERE user_id IN (SELECT id FROM users WHERE id = ?) AND category = ? AND id IN (SELECT task_id FROM archieve WHERE status = 'active');",session["user_id"],category)
     cat_lower = cat.lower()
-    
     if category == "House":
         house = db.execute("SELECT COUNT(category) AS house FROM tasks WHERE category = 'House' AND user_id IN (SELECT id FROM users WHERE id = ?) AND id IN (SELECT task_id FROM archieve WHERE status = 'active');",session["user_id"])
         return render_template("preview.html",records = row,cat = cat, h = house,cat_lower = cat_lower)
-        
     elif category == "Work":
         work = db.execute("SELECT COUNT(category) AS work FROM tasks WHERE category = 'Work' AND user_id IN (SELECT id FROM users WHERE id = ?) AND id IN (SELECT task_id FROM archieve WHERE status = 'active');",session["user_id"])
         return render_template("preview.html",records = row,cat = cat, w = work,cat_lower = cat_lower)
-      
     elif category == "Personal":  
         personal = db.execute("SELECT COUNT(category) AS personal FROM tasks WHERE category = 'Personal' AND user_id IN (SELECT id FROM users WHERE id = ?) AND id IN (SELECT task_id FROM archieve WHERE status = 'active');",session["user_id"])
         return render_template("preview.html",records = row,cat = cat, p = personal, cat_lower = cat_lower)
    
-
 ### TASK ###
 @app.route('/add' ,methods = ["POST", "GET"])
 @login_required
@@ -144,9 +138,15 @@ def add():
         task = request.form.get("task")
         category = request.form.get("category")
         date = request.form.get("date")
-        
         if not task or not category or not date:
             flash("Must provide data!","danger")
+            return redirect("/")
+# task validation       
+        if len(task) > 25:
+            flash("Task have more than 25 characters !","danger")
+            return redirect("/")
+        if task.isspace():
+            flash("Invalid task !","danger")
             return redirect("/")
 # insert task 
         db.execute("INSERT INTO tasks (task,category,created_date,due_date,user_id) VALUES(?,?,DATE(),?,?);", task,category,date,session["user_id"])
@@ -156,7 +156,6 @@ def add():
         flash("Added task!","success")
         return redirect("/")
 
-
 @app.route('/delete<id>', methods = ["POST"])
 @login_required
 def delete(id):
@@ -165,7 +164,6 @@ def delete(id):
     flash("Task deleted!","success")
     return redirect('/')
 
-
 @app.route('/done<id>', methods = ["POST"])
 @login_required
 def done(id):
@@ -173,15 +171,12 @@ def done(id):
     flash("Task completed!","success")
     return redirect('/')
 
-
 @app.route('/done_cat<id>', methods = ["POST"])
 @login_required
 def done_cat(id):
     db.execute("UPDATE archieve SET status = 'done', archieved = DATE() WHERE task_id = ?;",id )
-    #db.execute("DELETE FROM tasks WHERE id = ?",id)
     flash("Task completed!","success")
     return redirect('/')
-
 
 #UPDATE TASK FROM EDIT PAGE
 @app.route('/change<id_user>', methods = ["GET", "POST"])
@@ -194,7 +189,6 @@ def change(id_user):
         db.execute("UPDATE tasks SET task = ?,category = ?,due_date = ? WHERE id = ?;",task,category,date,id_user)
         return redirect(url_for('index'))
     
-    
 @app.route('/edit<task>:<category>:<data>:<id>', methods = ["GET", "POST"])
 @login_required
 def edit(task,category,data,id):
@@ -204,13 +198,11 @@ def edit(task,category,data,id):
     else:
         return redirect("/")
     
-
 @app.route('/history')
 @login_required
 def history():
         r = db.execute("SELECT * FROM archieve INNER JOIN tasks ON tasks.id = archieve.task_id WHERE tasks.user_id = ? AND archieve.status != 'active';",session["user_id"])
         return render_template('history.html',record = r)
-    
     
 @app.route('/restore<id>', methods = ["GET", "POST"])
 @login_required
@@ -218,7 +210,6 @@ def restore(id):
      if request.method == "POST":
         db.execute("UPDATE archieve SET status = 'active' WHERE task_id IN (SELECT id FROM tasks WHERE user_id IN (SELECT id FROM users WHERE id = ?) AND id = ?)",session["user_id"],id)
         return redirect('/')
-
 
 @app.route('/shuffle')
 @login_required
@@ -235,3 +226,4 @@ def shuffle():
 def clear():
     db.execute("DELETE FROM archieve WHERE status != 'active' AND task_id IN (SELECT id FROM tasks WHERE user_id = ?);",session["user_id"])
     return redirect('/')
+
